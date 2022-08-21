360195f3e1f6d7b15b15b83dedb9353a3aca8b8d
wada <wadayugo@keio.jp>
2022-08-21 20:14:16 +0900

    apply style

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,9 @@ function App() {
 
   return (
     <div className="App">
-      <div className="header-container">
-        <h1>Char Counter</h1>
-      </div>
       <div className="input-area-container">
         <h3>Input Area</h3>
+        <p>Please input text that you want to know character count</p>
         <textarea name="input-area" id="input-area" onChange={(e) => {onChangeCallback(e)}}></textarea>
       </div>
       <div className='count-area-container'>
